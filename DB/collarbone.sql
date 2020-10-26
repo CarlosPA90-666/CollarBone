45094ae42a576ccccec068a63f3dbb37c7edd519
ACLXRD <andres.lopez01@correo.usa.edu.co>
2020-10-25 21:59:54 -0500

    Se agrega css de camiseta

--- DB/collarbone.sql
+++ DB/collarbone.sql
@@ -2,10 +2,13 @@ create database Collarbone;
 use Collarbone;
 
 create table usuarios (
-	cc int not null,
     nombre varchar (49) not null,
     apellido varchar (49) not null,
     correo varchar (49) not null,
-    password_ varchar (10) not null,
-    primary key (cc)
+    password_ varchar (100) not null,
+    primary key (correo)
 );
+
+
+select * from usuarios;
+DELETE FROM `Collarbone`.`usuarios` WHERE (`correo` = 'andrescamilolr@gmail.com');
