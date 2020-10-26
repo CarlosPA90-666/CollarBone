ea11c85b17ccdf4b0f6305dc01622449422d0b73
root <root@ip-172-31-59-92.ec2.internal>
2020-10-26 20:29:57 +0000

    Administracion (faltan iconos)

--- js/myjs.js
+++ js/myjs.js
@@ -1,4 +1,3 @@
-console.log('antes');
 home();
 
 const btn = document.querySelector('#menu-btn');
@@ -39,6 +38,97 @@ function home(){
 	//-------------------Crear Camiseta-----------------------
 	document.getElementById('CrearCamiseta').style.display="none";
 	//-------------------Crear Camiseta-----------------------
-	console.log('aaa');
+
+	//-------------------Modificar Camiseta-----------------------
+        document.getElementById('ModificarCamiseta').style.display="none";
+	//-------------------Modificar Camiseta-----------------------
+
+	//-------------------Buscar Camiseta-----------------------
+        document.getElementById('BuscarCamiseta').style.display="none";
+        //-------------------Buscar Camiseta-----------------------
+
+	//-------------------Eliminar Camiseta-----------------------
+        document.getElementById('EliminarCamiseta').style.display="none";
+	//-------------------Eliminar Camiseta-----------------------
 
 }
+
+function mostrarCrearCamisetas(){
+
+	 document.getElementById('CrearCamiseta').style.display="block";
+       	 document.getElementById('ModificarCamiseta').style.display="none";
+	 document.getElementById('BuscarCamiseta').style.display="none";
+	 document.getElementById('EliminarCamiseta').style.display="none";
+
+}
+
+function mostrarModificarCamisetas(){
+
+         document.getElementById('CrearCamiseta').style.display="none";
+         document.getElementById('ModificarCamiseta').style.display="block";
+         document.getElementById('BuscarCamiseta').style.display="none";
+         document.getElementById('EliminarCamiseta').style.display="none";
+
+}
+
+function mostrarBuscarCamisetas(){
+
+         document.getElementById('CrearCamiseta').style.display="none";
+         document.getElementById('ModificarCamiseta').style.display="none";
+         document.getElementById('BuscarCamiseta').style.display="block";
+         document.getElementById('EliminarCamiseta').style.display="none";
+
+}
+
+function mostrarEliminarCamisetas(){
+
+         document.getElementById('CrearCamiseta').style.display="none";
+         document.getElementById('ModificarCamiseta').style.display="none";
+         document.getElementById('BuscarCamiseta').style.display="none";
+         document.getElementById('EliminarCamiseta').style.display="Block";
+
+}
+
+
+function Salir(){
+	location.replace("InicioSesion.html");
+}
+
+$("#buttonSalir").click(function(){
+
+        Salir();
+	Home();
+
+});
+
+
+$("#buttonEspacios").click(function(){
+
+	mostrarCrearCamisetas();
+
+});
+
+$("#buttonHabitos").click(function(){
+
+        mostrarModificarCamisetas();
+
+});
+
+$("#buttonTutorias").click(function(){
+
+        mostrarBuscarCamisetas();
+
+});
+
+$("#buttonHorarios").click(function(){
+
+        mostrarEliminarCamisetas();
+
+});
+
+$("#buttonLogin").click(function(){
+
+        mostrarEliminarCamisetas();
+
+});
+
