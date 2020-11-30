2409570850abb08403a7029a05da4e3026cde52f
ACLXRD <andres.lopez01@correo.usa.edu.co>
2020-11-29 19:22:16 -0500

    Se agrega sección de Vue en el front de admin

--- js/axios.js
+++ js/axios.js
@@ -3,27 +3,14 @@ var datos
 var app = new Vue({
   el: '#app',
   data: {
-    user: [],
-    onlyUser: [],
-    supp: []
+    datos: []
   },
   methods: {
     peticion: function () {
       axios.get('http://ec2-3-93-6-146.compute-1.amazonaws.com:5000/BuscarC')
         .then(response => {
-          this.data = response.data.data
-          console.log(this.data)
-        })
-        .catch(error => console.error(error))
-    },
-    p: function (id) {
-      axios.get('http://ec2-3-93-6-146.compute-1.amazonaws.com:5000/BuscarC')
-        .then(response => {
-          datos = response.data
-          this.supp = datos.support
-          this.onlyUser = datos.data
-          console.log(this.supp)
-          $('#exampleModal').modal('show')
+          this.datos = response.data.data
+          console.log(this.datos)
         })
         .catch(error => console.error(error))
     }
