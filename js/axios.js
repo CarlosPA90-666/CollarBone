49c3ec95fad977567d5c486bd38f3813262d0a61
ACLXRD <andres.lopez01@correo.usa.edu.co>
2020-11-30 17:09:49 -0500

    Complementos para eliminar camiseta con axios

--- js/axios.js
+++ js/axios.js
@@ -1,3 +1,4 @@
+var url = 'http://ec2-3-92-45-110.compute-1.amazonaws.com:5000'
 var axios
 var datos
 var unC
@@ -12,7 +13,7 @@ var app = new Vue({
   },
   methods: {
     peticion: function () {
-      axios.get('http://ec2-3-92-45-110.compute-1.amazonaws.com:5000/BuscarC')
+      axios.get(url + '/BuscarC')
         .then(response => {
           this.datos = response.data.data
           console.log(this.datos)
@@ -20,7 +21,7 @@ var app = new Vue({
         .catch(error => console.error(error))
     },
     p: function () {
-      axios.get('http://ec2-3-92-45-110.compute-1.amazonaws.com:5000/BuscarCone')
+      axios.get(url + '/BuscarCone')
         .then(response => {
           this.unC = response.data.data
           console.log(app.unC)
@@ -33,30 +34,69 @@ var app = new Vue({
   }
 })
 
-document.getElementById("nomscam1").onchange=function() {x()};
+document.getElementById('nomscam1').onchange = function () { x() }
 
 function x () {
-    combo = document.getElementById("nomscam1");
-    selected = combo.options[combo.selectedIndex].text;
-    document.getElementById('nombreC').value = selected; 
-    console.log(selected)  
-    console.log(app.datos) 
+  combo = document.getElementById('nomscam1')
+  selected = combo.options[combo.selectedIndex].text
+  document.getElementById('nombreC').value = selected
+  console.log(selected)
+  console.log(app.datos)
 
-    for(const x of app.datos){
-    if (x['nombre'] == selected) {
-      console.log(x['nombre']);
-      document.getElementById("NombrecamisetaBusqueda").value=x['nombre']
-      document.getElementById("NumeroCamisetasDisponiblesBusqueda").value=x['cantidad']
-      document.getElementById("TallasDisponiblesBusqueda").value=x['tallas']
-      document.getElementById("PrecioCamisetaBusqueda").value=x['precio']
-      document.getElementById("ImagenCamisetaBusqueda").value=x['url'];
+  for (const x of app.datos) {
+    if (x.nombre === selected) {
+      console.log(x.nombre)
+      document.getElementById('NombrecamisetaBusqueda').value = x.nombre
+      document.getElementById('NumeroCamisetasDisponiblesBusqueda').value = x.cantidad
+      document.getElementById('TallasDisponiblesBusqueda').value = x.tallas
+      document.getElementById('PrecioCamisetaBusqueda').value = x.precio
+      document.getElementById('ImagenCamisetaBusqueda').value = x.url
     }
-
-  }                             
-    }
+  }
+}
 
 $('#btn Buscarcam').click(function () {
   x()
- 
 })
 
+var pepe = new Vue({
+  el: '#pepe',
+  data: {
+    datos: [],
+    unC: []
+  },
+  methods: {
+    peticion: function () {
+      axios.get(url + '/BuscarC')
+        .then(response => {
+          this.datos = response.data.data
+          console.log(this.datos)
+        })
+        .catch(error => console.error(error))
+    },
+    eliminar: function () {
+      axios.delete(url + '/BorrarC', { headers: {}, data: { supC: this.supC } })
+        .then((response) => {
+          console.log(response)
+          this.peticion()
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+                    console.log(error.request);
+          this.peticion()
+        })
+      this.clear()
+    }
+  },
+  created: function () {
+    this.peticion()
+  }
+})
+
+function y () {
+  combo = document.getElementById('nomscam2')
+  selected = combo.options[combo.selectedIndex].text
+  document.getElementById('borrarC').value = selected
+  console.log(selected)
+  console.log(app.datos)
+}
