89f1b3cdc8cb9ecf36f0ae7745e74b36e3494d19
ACLXRD <andres.lopez01@correo.usa.edu.co>
2020-11-29 22:36:28 -0500

    Se trabaja en sección de busqueda

--- js/axios.js
+++ js/axios.js
@@ -18,9 +18,9 @@ var app = new Vue({
         .catch(error => console.error(error))
     },
     p: function () {
-      axios.get('http://ec2-3-93-6-146.compute-1.amazonaws.com:5000/BuscarC' + this.nombreT)
+      axios.get('http://ec2-3-93-6-146.compute-1.amazonaws.com:5000/BuscarCone')
         .then(response => {
-          this.unC = datos.data
+          this.unC = response.data.data
           console.log(this.unC)
         })
         .catch(error => console.error(error))
